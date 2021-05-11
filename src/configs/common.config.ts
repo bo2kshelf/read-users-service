9b488b3b25e54b2e594e91d7ccf9bf79dd6e70ce
SnO₂WMaN <sno2wman.official@gmail.com>
2021-05-11 12:29:52 +0900

    feat: currentUser用にユーザーを強制的にいる状態にする (#4)

--- src/configs/common.config.ts
+++
@@ -1,5 +0,0 @@
-import {registerAs} from '@nestjs/config';
-
-export const CommonConfig = registerAs('common', () => ({
-  imageproxyBaseUrl: process.env.IMAGEPROXY_BASE_URL,
-}));
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,43 +1,27 @@
-import {Inject} from '@nestjs/common';
-import {ConfigType} from '@nestjs/config';
-import {
-  Args,
-  Parent,
-  Query,
-  ResolveField,
-  Resolver,
-  ResolveReference,
-} from '@nestjs/graphql';
-import {CommonConfig} from '../configs/common.config';
+import {NotFoundException} from '@nestjs/common';
+import {Args, Query, Resolver, ResolveReference} from '@nestjs/graphql';
 import {FindUserArgs} from './dto/find-user.dto';
 import {UserEntity} from './user.entity';
 import {UsersService} from './users.service';
 
 @Resolver(() => UserEntity)
 export class UsersResolver {
-  constructor(
-    @Inject(CommonConfig.KEY)
-    private readonly config: ConfigType<typeof CommonConfig>,
-    private readonly usersService: UsersService,
-  ) {}
+  constructor(private readonly usersService: UsersService) {}
 
   @ResolveReference()
-  resolveReference(reference: {__typename: string; id: string}) {
-    return this.usersService.findUser({id: reference.id});
-  }
-
-  @ResolveField(() => String)
-  picture(@Parent() {picture}: UserEntity) {
-    return new URL(`/${picture}`, this.config.imageproxyBaseUrl).toString();
+  resolveReference({id}: {id: string}): Promise<UserEntity> {
+    return this.usersService.ensureUser({id});
   }
 
   @Query(() => UserEntity, {name: 'user'})
-  async findUser(@Args() args: FindUserArgs) {
-    return this.usersService.findUser(args);
+  async findUser(@Args() {uniqueName}: FindUserArgs): Promise<UserEntity> {
+    const user = await this.usersService.findUser(uniqueName);
+    if (!user) throw new NotFoundException();
+    return user;
   }
 
   @Query(() => [UserEntity])
-  async allUsers() {
+  async allUsers(): Promise<UserEntity[]> {
     return this.usersService.allUsers();
   }
 }
